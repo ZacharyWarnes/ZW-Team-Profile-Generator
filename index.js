7bab41b9afd38b7481a232f1b857fa7b564041f4
Zachary Warnes <zacharywarnes@gmail.com>
2022-09-03 13:00:31 -0700

    adding some psuedo code

---
+++ index.js
@@ -0,0 +1,34 @@
+// Require 'inquirer
+
+
+//Write functions that call each other instead of create For Loops
+
+//Function that garthers manager data 
+        // inquirer.prompt()
+            //What is the team manager's name?
+            //What is the team manager's id?
+            //What is the team manager's email?
+            //What is the team manager's office number?
+        //THEN Build a manager object
+
+
+// Gather engineer data
+        // inquirer.prompt()
+            //What is your engineer's name?
+            //what is yoru engineer's id?
+            //What is your engineer's email?
+            //What is your engineer's GitHub username?
+        //THEN build an engineer object 
+
+// Gather Intern data
+        // inquirer.prompt()
+            //What is your intern's name?
+            //What is your intern's id?
+            //What is your intern's email?
+            //What is your intern's school?
+        //THEN Build an intern object
+
+//What type of team member would you like to add?
+        //Manager
+        //Engineer
+        //Intern
