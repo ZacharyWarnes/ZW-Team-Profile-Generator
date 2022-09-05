e07ad50cd978221ac3a938d396bf675208732016
Zachary Warnes <zacharywarnes@gmail.com>
2022-09-05 11:08:59 -0700

    adding template literal for HTML page

--- src/page-template.js
+++ src/page-template.js
@@ -1,4 +1,4 @@
-function createHTML() {
+function createHTML(managerProfile, engineerProfiles, internProfiles) {
     templateHTML= `
     <!DOCTYPE html>
 <html lang="en">
@@ -10,13 +10,31 @@ function createHTML() {
 
     <title>ZW TEAM PROFILE GENERATOR</title>
 </head>
+
 <body>
-  
+<div class="jumbotron jumbotron-fluid bg-danger">
+    <div class="container mx-auto">
+        <h1 class="text-center font-weight-bold text-white">MY TEAM</h1>
+    </div>
+</div>
+<div class="container-fluid col">
+    <div class="card-deck">
+        ${managerProfile}
+        ${engineerProfiles}
+        ${internProfiles}
+    </div>
+</div>
+
+
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 
 </body>
 </html>
-    `
+    `;
+
+    return templateHTML;
 }
+
+module.exports = createHTML;
