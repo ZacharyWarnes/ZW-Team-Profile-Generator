1592843ccc138500068db9bcff8c3274d4591de8
Zachary Warnes <zacharywarnes@gmail.com>
2022-09-05 12:40:34 -0700

    fixing email links and gitHub link in template literal

--- index.js
+++ index.js
@@ -188,7 +188,7 @@ function generateManagerProfile() {
         <div class="card-body">
             <ul class="list-group list-group-flush">
                 <li class="list-group-item">ID: ${id}</li>
-                <li class="list-group-item">Email: ${email}</li>
+                <li class="list-group-item">Email: <a href="mailto:${email}" target="_blank">${email}</a></li>
                 <li class="list-group-item">Office number: ${officeNumber}</li>
             </ul>
         </div>
@@ -208,8 +208,8 @@ function createEngineerProfile(name, id, email, github, role) {
         <div class="card-body">
             <ul class="list-group list-group-flush">
                 <li class="list-group-item">ID: ${id}</li>
-                <li class="list-group-item">Email: ${email}</li>
-                <li class="list-group-item">GitHub: <a href= "https://github.com/${github}"></li>
+                <li class="list-group-item">Email: <a href="mailto:${email}" target="_blank">${email}</a></li>
+                <li class="list-group-item">GitHub: <a href="https://github.com/${github}" target="_blank">${github}</a></li>
             </ul>
         </div>
     </div>
@@ -242,7 +242,7 @@ function createInternProfile(name, id, email, school, role) {
         <div class="card-body">
             <ul class="list-group list-group-flush">
                 <li class="list-group-item">ID: ${id}</li>
-                <li class="list-group-item">Email: ${email}</li>
+                <li class="list-group-item">Email: <a href="mailto:${email}" target="_blank">${email}</a></li>
                 <li class="list-group-item">School: ${school} </li>
             </ul>
         </div>
